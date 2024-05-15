d653337a047b1d91fbe20589b063712048162ed6
s3anfinnegan <sean2001finnegan@gmail.com>
2024-05-15 19:26:30 +0100

    Updates

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,6 +4,7 @@ import { FaGraduationCap, FaLaptop, FaLightbulb } from 'react-icons/fa';
 
 const Body = () => {
   return (
+    <div className="main">
     <div className="hero-text">
       <h1>Hi, I'm Seán</h1>
       <p>Fáilte go dtí mo leathanach tuirlingthe!</p>
@@ -13,6 +14,7 @@ const Body = () => {
         <li><FaGraduationCap /> Graduate of BEng (ECE) @ <a href="https://www.universityofgalway.ie/courses/undergraduate-courses/electronic-and-computers.html" target="_blank" rel="noopener noreferrer">University of Galway</a></li>
         <li><FaLightbulb /> Startup founder & engineer @ <a href="https://www.f6s.com/company/receipt-relay#about" target="_blank" rel="noopener noreferrer">Receipt Relay</a>, <a href="https://www.breakthroughmaths.ie/" target="_blank" rel="noopener noreferrer">Breakthrough Maths</a></li>
       </ul>
+    </div>
     </div>
     </div>
   );
