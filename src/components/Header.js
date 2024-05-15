ac8b7f5bbee1b06dbd43468fa2576e7ba72fe09e
s3anfinnegan <sean2001finnegan@gmail.com>
2024-05-15 12:19:05 +0100

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Header.css';
+// import logo from '/path/to/svg/'
 
 const Header = () => {
   return (
     <div className="header">
-      <img src="/logo.svg" alt="Logo" />
+      <div className='logo'>
+        {/* SVG logo goes here */}
+      {/* <img src={logo} alt="Logo" /> */}
+      </div>
       <div className="header-link">
       <a href="/resume.pdf" download>Download CV</a>
         <a href="https://linkedin.com/in/sean-finnegan-/">Connect</a>
